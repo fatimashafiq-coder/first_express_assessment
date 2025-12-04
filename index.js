b7e26d899b1205fbc23ae6d85871a7c73d099e7d
workfatima829 <workfatima829@gmail.com>
2025-12-04 23:17:26 +0500

    implemnt the http methods and using the bcrypt for hasing password

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const taskRoutes = require('./routes/tasks');
+const userRoutes = require('./routes/users');
 app.use(express.json());
 app.use('/', taskRoutes);
+app.use('/', userRoutes);
 
-app.listen(3001)
+app.listen(9000)
---
+++ routes/users.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const router = express.Router();
+let usersDetails = require('../usersDetails.json');
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
+
+const validateUser = (req, res, next) => {
+    if ((!req.body.name) || (!req.body.email) || (!req.body.password)) {
+        return res.status(400).json({ error: "Empty Body not allowed" });
+    }
+    next();
+};
+
+router.post('/userSignup', validateUser, (req, res) => {
+    const { name, email, password } = req.body;
+    const saltRounds = 10;
+    bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
+        if (err) {
+            return res.status(500).json({ error: "Error hashing password" });
+        }
+        const newUser = {
+            id: uuidv4(),
+            name,
+            email,
+            password: hashedPassword
+        };
+        usersDetails.push(newUser);
+        fs.writeFile('./usersDetails.json', JSON.stringify(usersDetails), (err) => {
+            if (err) {
+                return res.status(500).json({ error: "Error saving user data" });
+            }
+            return res.status(201).send({
+                message: "User Signup Successfully",
+            });
+        });
+    });
+});
+
+
+router.post('/userLogin', (req, res) => {
+    const { email, password } = req.body;
+    const user = usersDetails.find(u => u.email === email);
+    if (!user) {
+        return res.status(401).send({ message: "User not found" });
+    }
+     bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) {
+            return res.status(500).send({ message: "Error comparing passwords" });
+        }
+        if (!isMatch) {
+            return res.status(401).send({ message: "Invalid password" });
+        }
+        return res.status(200).send({ message: "Login successful" });
+    });
+
+});
+
+router.put("/update/:id", (req, res) => {
+    const id = req.params.id;
+    const { password } = req.body;
+    const userIndex = usersDetails.findIndex(user => user.id === id);
+
+    if (userIndex === -1) {
+        return res.status(404).send({ message: "User not found" });
+    }
+    if (password) {
+        const saltRounds = 10;
+        bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
+            if (err) {
+                return res.status(500).json({ error: "Error hashing password" });
+            }
+            usersDetails[userIndex].password = hashedPassword;
+            fs.writeFile('./usersDetails.json', JSON.stringify(usersDetails), (err) => {
+                if (err) {
+                    return res.status(500).json({ error: "Error saving user data" });
+                }
+                return res.status(200).send({
+                    message: "User updated successfully",
+                });
+            });
+        });
+    } else {
+        return res.status(400).send({ message: "Password is required to update" });
+    }
+});
+router.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    const userIndex = usersDetails.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+        return res.status(404).send("id not found")
+    }
+    const deletedUser = usersDetails.splice(userIndex, 1);
+    fs.writeFile('./usersDetails.json', JSON.stringify(usersDetails), (err) => {
+        return res.status(200).send(`delete successfully data`);
+    })
+})
+
+module.exports = router;
